9ab4e1bbddcdee039c7a935a45797746515abcb0
Jongwoo Kim <kjwoo810@gmail.com>
2022-04-04 18:54:12 +0900

    Add Bottom_Sheet UI

---
+++ app/src/main/java/com/example/beermap/BottomSheetFragment.kt
@@ -0,0 +1,18 @@
+package com.example.beermap
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class BottomSheetFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.bottom_sheet, container, false)
+        return view
+    }
+}
--- app/src/main/java/com/example/beermap/MainActivity.kt
+++ app/src/main/java/com/example/beermap/MainActivity.kt
@@ -2,10 +2,79 @@ package com.example.beermap
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheet: LinearLayout
+    private lateinit var btnPersistent: Button
+    private lateinit var bottomSheetStateText: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+        setBtnExpandSheet()
+
+        val sheetBehavior = BottomSheetBehavior.from(bottomSheet)
+
+        btnPersistent.setOnClickListener {
+            if (sheetBehavior.state != BottomSheetBehavior.STATE_EXPANDED) {
+                sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+                setBtnCloseSheet()
+            } else {
+                sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+                setBtnExpandSheet()
+            }
+        }
+
+
+        sheetBehavior.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                //
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                        bottomSheetStateText.text = "HIDDEN"
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        setBtnCloseSheet()
+                        bottomSheetStateText.text = "EXPANDED"
+                    }
+                    BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        setBtnExpandSheet()
+                        bottomSheetStateText.text = "COLLAPSED"
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                }
+            }
+        })
+    }
+    private fun initView() {
+        bottomSheet = findViewById(R.id.bottomSheet)
+        btnPersistent = findViewById(R.id.btnPersistent)
+        bottomSheetStateText = findViewById(R.id.bottomSheetStateText)
+        bottomSheetStateText.text = "COLLAPSED"
+
+    }
+
+    private fun setBtnExpandSheet() {
+        btnPersistent.text = "Expand sheet"
+    }
+
+    private fun setBtnCloseSheet() {
+        btnPersistent.text = "Close sheet"
     }
 }
