7181e427a458c036db2c29295247b10d48ab3a09
Jongwoo Kim <kjwoo810@gmail.com>
2022-04-08 02:56:54 +0900

    Changes scheme data in firebase-RealtimeDB

--- app/src/main/java/com/example/beermap/MainActivity.kt
+++ app/src/main/java/com/example/beermap/MainActivity.kt
@@ -1,30 +1,44 @@
 package com.example.beermap
 
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.LinearLayout
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.beermap.firebase.PubData
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.getValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
 
 private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var bottomSheet: LinearLayout
+    private lateinit var bottomSheet: ConstraintLayout
     private lateinit var recyclerView: RecyclerView
     private lateinit var database : FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
     private lateinit var map: GoogleMap
+    private lateinit var geocoder: Geocoder
+    private val pubDataList: MutableList<PubData> = mutableListOf()
+    private val markerDataList: MutableList<LatLng> = mutableListOf()
+    private val pubMap: MutableMap<String, PubData> = mutableMapOf()
 
+    private lateinit var button: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,18 +49,32 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
+        geocoder = Geocoder(this@MainActivity, Locale.KOREA)
         initView()
 
         recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+        button.setOnClickListener {
+            for (i in pubMap) {
+                Log.d(TAG, "${i.key} : ${(i.value)}")
+                val geo = geocoder.getFromLocationName(i.value.address, 1)
+                updateData(i.key, i.value.address, i.value.menu, i.value.name, geo[0].latitude, geo[0].longitude)
+            }
+        }
 
         databaseReference.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                val pubDataList: MutableList<PubData> = mutableListOf()
                 for (data in snapshot.children) {
                     val result = data.getValue<PubData>()
                     pubDataList.add(result!!)
-                    Log.d(TAG, "${result?.name}")
+                    Log.d(TAG, "${data.key}")
+                    pubMap.put(data.key!!, result)
+                }
+                pubDataList.forEach { pubData ->
+                    CoroutineScope(Dispatchers.Main).launch {
+                        val geo = geocoder.getFromLocationName(pubData.address, 1)
+                        val pubLatLng = LatLng(geo[0].latitude, geo[0].longitude)
+                        markerDataList.add(pubLatLng)
+                    }
                 }
                 recyclerView.adapter = PubdataRecyclerViewAdapter(this@MainActivity, pubDataList)
             }
@@ -87,10 +115,31 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        val seoul = LatLng(37.1, 128.0)
+        // 카메라 이동
+        map.moveCamera(CameraUpdateFactory.newLatLng(seoul))
+
+    }
+    private fun updateData(pubNo: String, address: String, menu: String, name: String, Lat: Double, Lng: Double){
+        val pub: Map<String, Any> = mapOf("address" to address, "menu" to menu, "name" to name, "Lat" to Lat, "Lng" to Lng)
+        databaseReference.child(pubNo).updateChildren(pub)
+    }
+
+    private fun addMarkers(googleMap: GoogleMap) {
+        map = googleMap
+        pubDataList.forEach { pubData ->
+            val geo = geocoder.getFromLocationName(pubData.address, 1)
+            val pubLatLng = LatLng(geo[0].latitude, geo[0].longitude)
+            map.addMarker(MarkerOptions()
+                .position(pubLatLng)
+                .title(pubData.name)
+            )
+        }
     }
 
     private fun initView() {
         bottomSheet = findViewById(R.id.bottomSheet)
         recyclerView = findViewById(R.id.recyclerView)
+        button = findViewById(R.id.button)
     }
 }
--- app/src/main/java/com/example/beermap/firebase/PubData.kt
+++ app/src/main/java/com/example/beermap/firebase/PubData.kt
@@ -3,6 +3,6 @@ package com.example.beermap.firebase
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
-data class PubData(var address: String, var name: String, var menu: String) {
-    constructor() : this("", "", "")
+data class PubData(var address: String, var name: String, var menu: String, var Lat: Double, var Lng: Double) {
+    constructor() : this("", "", "", 0.0, 0.0)
 }
