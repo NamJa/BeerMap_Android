0352993fdd7f800e1ca1494a954171243a033993
Jongwoo Kim <kjwoo810@gmail.com>
2022-04-14 23:17:36 +0900

    Receive & Show my GPS Location only on GPS Button Clicked

--- app/src/main/java/com/example/beermap/MainActivity.kt
+++ app/src/main/java/com/example/beermap/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.beermap
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.location.Geocoder
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -14,12 +17,17 @@ import android.view.animation.AnimationUtils
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.widget.ThemeUtils
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.WindowCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.beermap.firebase.PubData
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -33,9 +41,9 @@ import com.google.firebase.database.ktx.getValue
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.util.*
 
 private const val TAG = "MainActivity"
+private const val LOCATION_PERMISSIONS_REQUEST = 1000
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -56,9 +64,12 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var database : FirebaseDatabase
     private lateinit var databaseReference: DatabaseReference
     private lateinit var map: GoogleMap
-    private lateinit var geocoder: Geocoder
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
     private var pubDataList: MutableList<PubData> = mutableListOf()
-
+    private var userCurLat: Double = 0.0
+    private var userCurLng: Double = 0.0
+    private var isNotEnabledGPS: Boolean = true
+    private var isMapZoomed: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -74,7 +85,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        geocoder = Geocoder(this@MainActivity, Locale.KOREA)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
         initView()
 
         recyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
@@ -86,7 +97,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 for (data in snapshot.children) {
                     val result = data.getValue<PubData>()
                     pubDataList.add(result!!)
-                    Log.d(TAG, "${data.key}")
                 }
                 CoroutineScope(Dispatchers.Main).launch {
                     pubDataList.forEach { pubData ->
@@ -147,6 +157,25 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 .addToBackStack(null)
                 .commit()
         }
+
+        //GPS 버튼 동작
+        mapGPSButton.setOnClickListener{
+            if(ActivityCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                onCheckPermission()
+            } else {
+                // 여기에 위치정보 받아오는 역할
+                if (isNotEnabledGPS) { // GPS가 비활성화 되어있다면
+                    requestCurrentLocation()
+                    mapGPSButton.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.gray_400))
+                    isNotEnabledGPS = false
+                } else { // GPS가 활성화 되어있다면
+                    fusedLocationClient.removeLocationUpdates(locationCallback)
+                    mapGPSButton.setBackgroundColor(ContextCompat.getColor(this@MainActivity, R.color.teal_200))
+                    isNotEnabledGPS = true
+                }
+            }
+        }
     }
 
 
@@ -159,9 +188,59 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
-    private fun updateData(pubNo: String, address: String, menu: String, name: String, Lat: Double, Lng: Double){
-        val pub: Map<String, Any> = mapOf("address" to address, "menu" to menu, "name" to name, "Lat" to Lat, "Lng" to Lng)
-        databaseReference.child(pubNo).updateChildren(pub)
+    override fun onDestroy() {
+        super.onDestroy()
+        // 위치 업데이트를 할 필요 없는 시점
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+
+    private fun onCheckPermission() {
+        if (ActivityCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            || ActivityCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@MainActivity, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Toast.makeText(this@MainActivity, "현재 위치를 받기 위해선 권한을 허용해야 합니다.", Toast.LENGTH_SHORT).show()
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSIONS_REQUEST)
+            } else {
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSIONS_REQUEST)
+            }
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun requestCurrentLocation() {
+        val locationRequest = com.google.android.gms.location.LocationRequest.create().apply {
+            priority = com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+            interval = 50
+            fastestInterval = 20
+        }
+        // 다른 Map app과 같이 현재 위치를 표시한다.
+        map.isMyLocationEnabled = true
+        map.uiSettings.isMyLocationButtonEnabled = true
+        // 기능 설정 및 ui 설정도 해야 화면에 파란 마커가 표시된다.
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this@MainActivity)
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper()!!)
+    }
+
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val lastLocation: Location = locationResult.lastLocation
+            userCurLat = lastLocation.latitude
+            userCurLng = lastLocation.longitude
+
+            // 최초 한 번만 gps 버튼을 눌렀을 때 확대되면서 현재 위치를 보여줌
+            val newLatLng = if (isMapZoomed) {
+                CameraUpdateFactory.newLatLng(LatLng(userCurLat, userCurLng))
+            } else {
+                isMapZoomed = true
+                CameraUpdateFactory.newLatLngZoom(LatLng(userCurLat, userCurLng), 16f)
+            }
+            map.animateCamera(newLatLng)
+            Log.d(TAG, "latitude: $userCurLat")
+            Log.d(TAG, "longitude: $userCurLng")
+        }
     }
 
 
@@ -190,7 +269,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             this@MainActivity.navigationHeight()
         )
 
-
     }
 
     fun Context.navigationHeight(): Int {
@@ -218,8 +296,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                 item.setOnClickListener {
                     //test
                     Toast.makeText(this@MainActivity, "${pubData.menu}", Toast.LENGTH_SHORT).show()
-                    val newLatingZoom = CameraUpdateFactory.newLatLngZoom(LatLng(pubData.Lat, pubData.Lng), 16f)
-                    map.animateCamera(newLatingZoom)
+                    val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(LatLng(pubData.Lat, pubData.Lng), 16f)
+                    map.animateCamera(newLatLngZoom)
                     sheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
                 }
             }
