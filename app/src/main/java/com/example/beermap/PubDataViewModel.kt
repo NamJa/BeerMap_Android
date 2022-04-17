8d2bed9b3fd4058e59245a4d0124819a2e4df1ca
Jongwoo Kim <kjwoo810@gmail.com>
2022-04-18 01:24:23 +0900

    FIX: bottomSheet RecyclerView Problem: Re-printed list item #1

--- app/src/main/java/com/example/beermap/PubDataViewModel.kt
+++ app/src/main/java/com/example/beermap/PubDataViewModel.kt
@@ -1,19 +1,27 @@
 package com.example.beermap
 
-import android.widget.Toast
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BaseObservable
-import androidx.databinding.BindingAdapter
 import com.example.beermap.firebase.PubData
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class PubDataViewModel() : BaseObservable() {
     var pub: PubData? = null
     set(pub) {
         field = pub
+        // RecyclerView.ViewHolder bind() 함수에서
+        // 이 모델의 pub객체를 변경했음을 알 수 있는 방법이 없기 때문에
+        notifyChange()
+        // 이 함수를 사용하여 데이터 객체인 PubData의 모든 binding 속성값이 변경되었음을 바인딩 클래스에 알린다.
+        // 특정 바인딩 속성값만 변경이 필요하다면
+        /**
+         *
+        @get:Bindable
+        var firstName: String = ""
+            set(value) {
+                field = value
+                notifyPropertyChanged(BR.firstName)
+        }
+        * */
+        // 이런식으로 사용하면 된다.
     }
     val pubTitle: String?
         get() = pub?.name
